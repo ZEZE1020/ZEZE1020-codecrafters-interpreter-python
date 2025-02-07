86961c03a0d1bed19413347decc89e2c97c03535
ZEZE1020 <ogembogodfrey75@gmail.com>
2025-02-07 13:55:03 +0300

    Implement scanner

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ def main():
             print("LEFT_PAREN ( null")
         if c == ")":
             print("RIGHT_PAREN ) null")
-    print("EOF null")
+    print("EOF  null")
 
 if __name__ == "__main__":
     main()
